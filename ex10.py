5df3f5ebb8fe2c486c9ff68f687ed024bee6523a
Christopher S. Ornelas <68980556+ChristopherOrnelas@users.noreply.github.com>
2021-05-04 03:57:41 -0300

    Ex10, 11 completos

--- ex10.py
+++ ex10.py
@@ -4,13 +4,14 @@ Find the sum of all the primes below two million.'''
 
 
 def sumPrimes(n):
-    sum, sieve = 0, [True] * n
+    total = 0
+    sieve = [True] * n
     for p in range(2, n):
         if sieve[p]:
-            sum += p
+            total += p
             for i in range(p*p, n, p):
                 sieve[i] = False
-    return sum
+    return total
 
 
 print(sumPrimes(2000000))
